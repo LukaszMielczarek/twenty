dffdab4629bc295c13195d9d6e778b361738f50f
Lukasz Mielczarek <mielczarek.lukasz.karol@gmail.com>
2024-10-13 19:31:59 +0200

    feat: (ROOFIO-68) Add pagination

--- packages/twenty-front/src/modules/activities/template/components/TemplateItemsTable.tsx
+++ packages/twenty-front/src/modules/activities/template/components/TemplateItemsTable.tsx
@@ -1,131 +1,144 @@
-import { DataGrid, GridRowSelectionModel } from '@mui/x-data-grid';
-import { useColumns } from '@/activities/template/hooks/useColumns';
-import { useCachedProducts } from '@/activities/template/hooks/useCachedProducts';
-import { useRecoilState } from 'recoil';
-import { currentSelectedTemplateState } from '../states/currentSelectedTemplateState';
+import {DataGrid, GridPagination, GridRowSelectionModel} from '@mui/x-data-grid';
+import {useColumns} from '@/activities/template/hooks/useColumns';
+import {useCachedProducts} from '@/activities/template/hooks/useCachedProducts';
+import {useRecoilState} from 'recoil';
+import {currentSelectedTemplateState} from '../states/currentSelectedTemplateState';
 import styled from '@emotion/styled';
-import { Button } from '@/ui/input/button/components/Button';
+import {Button} from '@/ui/input/button/components/Button';
 import Grid from '@mui/material/Grid2';
-import { useEffect, useState } from 'react';
-import { TemplateItem } from '@/activities/types/Template';
-import { ThemeProvider, useTheme } from '@emotion/react';
-import { createTheme } from '@mui/material';
+import {useEffect, useState} from 'react';
+import {TemplateItem} from '@/activities/types/Template';
+import {ThemeProvider, useTheme} from '@emotion/react';
+import {createTheme} from '@mui/material';
 
 const StyledTemplateItemsTable = styled.div`
-  display: flex;
-  flex-direction: column;
+    display: flex;
+    flex-direction: column;
 `;
 
 const StyledTemplateItemsTableHeader = styled.div``;
 
 const StyledTemplateItemsTableContainer = styled.div`
-  display: flex;
-  flex-direction: column;
+    display: flex;
+    flex-direction: column;
+`;
+const StyledTemplatesTableFooter = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    padding: 1rem;
 `;
 
 export const TemplateItemsTable = () => {
-  const theme = useTheme();
-  const MuiTheme = createTheme({
-    palette: {
-      mode: theme.name === 'light' ? 'light' : 'dark',
-    },
-  });
-
-  const [currentSelectedTemplate, setCurrentSelectedTemplate] = useRecoilState(
-    currentSelectedTemplateState,
-  );
-  const [rows, setRows] = useState<TemplateItem[]>(
-      currentSelectedTemplate?.templateItems || [],
-  );
-  const [cachedProducts, updateCachedProducts] = useCachedProducts();
-  const columns = useColumns(cachedProducts);
-
-  useEffect(() => {
-    setRows(currentSelectedTemplate?.templateItems || []);
-    rows.forEach((row) => {
-      if (row.manufacturerId && row.categoryId) {
-        updateCachedProducts(row.manufacturerId, row.categoryId);
-      }
+    const theme = useTheme();
+    const MuiTheme = createTheme({
+        palette: {
+            mode: theme.name === 'light' ? 'light' : 'dark',
+        },
     });
-  }, [currentSelectedTemplate]);
 
-  const handleRowSelectionModelChange = (
-    rowSelectionModel: GridRowSelectionModel,
-  ) => {};
-
-  const handleProcessRowUpdate = (
-    updatedRow: TemplateItem,
-    originalRow: TemplateItem,
-  ) => {
-    if (originalRow.manufacturerId !== updatedRow.manufacturerId) {
-      updatedRow.categoryId = undefined;
-      updatedRow.productId = undefined;
-    } else if (originalRow.categoryId !== updatedRow.categoryId) {
-      updatedRow.productId = undefined;
-    }
-    const index = rows.findIndex((row) => row.id === originalRow.id);
-    const tempRows = [...rows];
-    tempRows[index] = updatedRow;
-    setRows(tempRows);
-    setCurrentSelectedTemplate({ ...currentSelectedTemplate, templateItems: tempRows });
-    return updatedRow;
-  };
-
-  const addNewRow = () => {
-    const tempItems: TemplateItem[] = [...rows];
-    tempItems.push({
-      id: self.crypto.randomUUID(),
-    });
-    setRows(tempItems);
-      setCurrentSelectedTemplate({ ...currentSelectedTemplate, templateItems: tempItems });
-  };
-
-  const getFooter = () => {
-    return (
-      <div>
-        <Grid container style={{ padding: '1rem' }}>
-          <Button
-            title="Add row"
-            onClick={() => {
-              addNewRow();
-            }}
-          ></Button>
-        </Grid>
-      </div>
+    const [currentSelectedTemplate, setCurrentSelectedTemplate] = useRecoilState(
+        currentSelectedTemplateState,
     );
-  };
+    const [rows, setRows] = useState<TemplateItem[]>(
+        currentSelectedTemplate?.templateItems || [],
+    );
+    const [cachedProducts, updateCachedProducts] = useCachedProducts();
+    const columns = useColumns(cachedProducts);
 
-  return (
-    <>
-      <StyledTemplateItemsTable>
-        <StyledTemplateItemsTableHeader>
-          <h3>Templates Items</h3>
-        </StyledTemplateItemsTableHeader>
-        <ThemeProvider theme={MuiTheme}>
-          <StyledTemplateItemsTableContainer>
-            <DataGrid
-              rows={rows}
-              columns={columns}
-              checkboxSelection={true}
-              disableRowSelectionOnClick
-              autoHeight={true}
-              processRowUpdate={handleProcessRowUpdate}
-              onRowSelectionModelChange={handleRowSelectionModelChange}
-              onCellEditStop={(event) => {
-                if (event.row.manufacturerId && event.row.categoryId) {
-                  updateCachedProducts(
-                    event.row.manufacturerId,
-                    event.row.categoryId,
-                  );
-                }
-              }}
-              slots={{
-                footer: getFooter,
-              }}
-            />
-          </StyledTemplateItemsTableContainer>
-        </ThemeProvider>
-      </StyledTemplateItemsTable>
-    </>
-  );
+    useEffect(() => {
+        setRows(currentSelectedTemplate?.templateItems || []);
+        rows.forEach((row) => {
+            if (row.manufacturerId && row.categoryId) {
+                updateCachedProducts(row.manufacturerId, row.categoryId);
+            }
+        });
+    }, [currentSelectedTemplate]);
+
+    const handleRowSelectionModelChange = (
+        rowSelectionModel: GridRowSelectionModel,
+    ) => {
+    };
+
+    const handleProcessRowUpdate = (
+        updatedRow: TemplateItem,
+        originalRow: TemplateItem,
+    ) => {
+        if (originalRow.manufacturerId !== updatedRow.manufacturerId) {
+            updatedRow.categoryId = undefined;
+            updatedRow.productId = undefined;
+        } else if (originalRow.categoryId !== updatedRow.categoryId) {
+            updatedRow.productId = undefined;
+        }
+        const index = rows.findIndex((row) => row.id === originalRow.id);
+        const tempRows = [...rows];
+        tempRows[index] = updatedRow;
+        setRows(tempRows);
+        setCurrentSelectedTemplate({...currentSelectedTemplate, templateItems: tempRows});
+        return updatedRow;
+    };
+
+    const addNewRow = () => {
+        const tempItems: TemplateItem[] = [...rows];
+        tempItems.push({
+            id: self.crypto.randomUUID(),
+        });
+        setRows(tempItems);
+        setCurrentSelectedTemplate({...currentSelectedTemplate, templateItems: tempItems});
+    };
+
+    const Footer = (props: any) => {
+        return (
+            <Grid container>
+                <StyledTemplatesTableFooter>
+                    <Button
+                        title="Add row"
+                        onClick={() => {
+                            addNewRow();
+                        }}
+                    ></Button>
+                    <GridPagination {...props}/>
+                </StyledTemplatesTableFooter>
+            </Grid>
+        );
+    };
+
+    return (
+        <>
+            <StyledTemplateItemsTable>
+                <StyledTemplateItemsTableHeader>
+                    <h3>Templates Items</h3>
+                </StyledTemplateItemsTableHeader>
+                <ThemeProvider theme={MuiTheme}>
+                    <StyledTemplateItemsTableContainer>
+                        <DataGrid
+                            rows={rows}
+                            columns={columns}
+                            checkboxSelection={true}
+                            disableRowSelectionOnClick
+                            autoHeight={true}
+                            processRowUpdate={handleProcessRowUpdate}
+                            onRowSelectionModelChange={handleRowSelectionModelChange}
+                            onCellEditStop={(event) => {
+                                if (event.row.manufacturerId && event.row.categoryId) {
+                                    updateCachedProducts(
+                                        event.row.manufacturerId,
+                                        event.row.categoryId,
+                                    );
+                                }
+                            }}
+                            pagination
+                            pageSizeOptions={[5, 10, 25]}
+                            initialState={{
+                                pagination: {paginationModel: {pageSize: 5}},
+                            }}
+                            slots={{
+                                footer: Footer,
+                            }}
+                        />
+                    </StyledTemplateItemsTableContainer>
+                </ThemeProvider>
+            </StyledTemplateItemsTable>
+        </>
+    );
 };
--- packages/twenty-front/src/modules/activities/template/components/TemplateTable.tsx
+++ packages/twenty-front/src/modules/activities/template/components/TemplateTable.tsx
@@ -1,173 +1,187 @@
-import { DataGrid, GridColDef, GridRowSelectionModel } from '@mui/x-data-grid';
-import { useRecoilState } from 'recoil';
-import { currentTemplatesState } from '../states/currentTemplatesState';
-import { currentSelectedTemplateState } from '../states/currentSelectedTemplateState';
+import {DataGrid, GridColDef, GridPagination, GridRowSelectionModel} from '@mui/x-data-grid';
+import {useRecoilState} from 'recoil';
+import {currentTemplatesState} from '../states/currentTemplatesState';
+import {currentSelectedTemplateState} from '../states/currentSelectedTemplateState';
 import styled from '@emotion/styled';
-import { Button } from '@/ui/input/button/components/Button';
+import {Button} from '@/ui/input/button/components/Button';
 import Grid from '@mui/material/Grid2';
-import { useEffect, useState } from 'react';
-import { Template } from '@/activities/types/Template';
-import { ThemeProvider, useTheme } from '@emotion/react';
-import { createTheme } from '@mui/material';
-import { UUID } from 'crypto';
+import {useEffect, useState} from 'react';
+import {Template} from '@/activities/types/Template';
+import {ThemeProvider, useTheme} from '@emotion/react';
+import {createTheme} from '@mui/material';
+import {UUID} from 'crypto';
 
 const StyledTemplateTable = styled.div`
-  display: flex;
-  flex-direction: column;
+    display: flex;
+    flex-direction: column;
 `;
 
 const StyledTemplateTableHeader = styled.div``;
 
 const StyledTemplatesTableContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  overflow: auto;
+    display: flex;
+    flex-direction: column;
+    overflow: auto;
 `;
 
-export const TemplateTable = ({warehouseId}:{warehouseId: UUID}) => {
-  const theme = useTheme();
-  const MuiTheme = createTheme({
-    palette: {
-      mode: theme.name === 'light' ? 'light' : 'dark',
-    },
-  });
+const StyledTemplatesTableFooter = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    padding: 1rem;
+`;
 
-  const [templates, setTemplates] = useRecoilState(currentTemplatesState);
-  const [selectedTemplate, setSelectedTemplate] = useRecoilState(
-    currentSelectedTemplateState,
-  );
-  const [rows, setRows] = useState<Template[]>(templates);
-
-  const columns: GridColDef[] = [
-    {
-      field: 'id',
-      headerName: 'ID',
-      editable: false,
-      type: 'string',
-      flex: 1,
-    },
-    {
-      field: 'name',
-      headerName: 'NAME',
-      editable: true,
-      type: 'string',
-      flex: 1,
-    },
-    {
-      field: 'description',
-      headerName: 'DESCRIPTION',
-      editable: true,
-      type: 'string',
-      flex: 1,
-    },
-    {
-      field: 'validFrom',
-      headerName: 'VALID FROM',
-      editable: true,
-      type: 'dateTime',
-      flex: 1,
-    },
-    {
-      field: 'validTo',
-      headerName: 'VALID TO',
-      editable: true,
-      type: 'dateTime',
-      flex: 1,
-    },
-  ];
-
-  useEffect(() => {
-    setRows(templates);
-  }, []);
-
-  const handleProcessRowUpdate = async (
-    updatedRow: Template,
-    originalRow: Template,
-  ) => {
-    let index = rows.findIndex((row) => row.id === originalRow.id);
-    let tempRows = [...rows];
-    tempRows[index] = updatedRow;
-    setRows(tempRows);
-    setSelectedTemplate(updatedRow);
-    return updatedRow;
-  };
-
-  const handleRowSelectionModelChange = (
-    rowSelectionModel: GridRowSelectionModel,
-  ) => {
-    if (rowSelectionModel.length > 0) {
-      const selectedTemplate = rows.find(
-        (template) => template.id === rowSelectionModel[0],
-      );
-      setSelectedTemplate(selectedTemplate ? { ...selectedTemplate } : null);
-    } else {
-      setSelectedTemplate(null);
-    }
-  };
-
-  const handleCellEditStart = (
-    params: any,
-    event: { stopPropagation: () => void },
-  ) => {
-    event.stopPropagation(); // Prevent deselection during editing
-  };
-
-  const addNewRow = () => {
-    let newRows = [...rows];
-    const newRow = {
-      id: self.crypto.randomUUID(),
-      warehouseId: warehouseId,
-      templateItems: [
-        {
-          id: self.crypto.randomUUID(),
+export const TemplateTable = ({warehouseId}: { warehouseId: UUID }) => {
+    const theme = useTheme();
+    const MuiTheme = createTheme({
+        palette: {
+            mode: theme.name === 'light' ? 'light' : 'dark',
         },
-      ],
-    };
-    newRows.push(newRow);
-    setRows(newRows);
-    setSelectedTemplate(newRow);
-  };
+    });
 
-  const getFooter = () => {
-    return (
-      <div>
-        <Grid container style={{ padding: '1rem' }}>
-          <Button
-            title="Add row"
-            onClick={() => {
-              addNewRow();
-            }}
-          ></Button>
-        </Grid>
-      </div>
+    const [templates, setTemplates] = useRecoilState(currentTemplatesState);
+    const [selectedTemplate, setSelectedTemplate] = useRecoilState(
+        currentSelectedTemplateState,
     );
-  };
+    const [rows, setRows] = useState<Template[]>(templates);
 
-  return (
-    <>
-      <StyledTemplateTable>
-        <StyledTemplateTableHeader>
-          <h3>Templates</h3>
-        </StyledTemplateTableHeader>
-        <ThemeProvider theme={MuiTheme}>
-          <StyledTemplatesTableContainer>
-            <DataGrid
-              rows={rows}
-              columns={columns}
-              autoHeight={true}
-              checkboxSelection={true}
-              disableMultipleRowSelection
-              disableRowSelectionOnClick
-              onCellEditStart={handleCellEditStart}
-              processRowUpdate={handleProcessRowUpdate}
-              onRowSelectionModelChange={handleRowSelectionModelChange}
-              slots={{
-                footer: getFooter,
-              }}
-            />
-          </StyledTemplatesTableContainer>
-        </ThemeProvider>
-      </StyledTemplateTable>
-    </>
-  );
+    const columns: GridColDef[] = [
+        {
+            field: 'id',
+            headerName: 'ID',
+            editable: false,
+            type: 'string',
+            flex: 1,
+        },
+        {
+            field: 'name',
+            headerName: 'NAME',
+            editable: true,
+            type: 'string',
+            flex: 1,
+        },
+        {
+            field: 'description',
+            headerName: 'DESCRIPTION',
+            editable: true,
+            type: 'string',
+            flex: 1,
+        },
+        {
+            field: 'validFrom',
+            headerName: 'VALID FROM',
+            editable: true,
+            type: 'dateTime',
+            flex: 1,
+        },
+        {
+            field: 'validTo',
+            headerName: 'VALID TO',
+            editable: true,
+            type: 'dateTime',
+            flex: 1,
+        },
+    ];
+
+    useEffect(() => {
+        setRows(templates);
+    }, []);
+
+    const handleProcessRowUpdate = async (
+        updatedRow: Template,
+        originalRow: Template,
+    ) => {
+        let index = rows.findIndex((row) => row.id === originalRow.id);
+        let tempRows = [...rows];
+        tempRows[index] = updatedRow;
+        setRows(tempRows);
+        setSelectedTemplate(updatedRow);
+        return updatedRow;
+    };
+
+    const handleRowSelectionModelChange = (
+        rowSelectionModel: GridRowSelectionModel,
+    ) => {
+        if (rowSelectionModel.length > 0) {
+            const selectedTemplate = rows.find(
+                (template) => template.id === rowSelectionModel[0],
+            );
+            setSelectedTemplate(selectedTemplate ? {...selectedTemplate} : null);
+        } else {
+            setSelectedTemplate(null);
+        }
+    };
+
+    const handleCellEditStart = (
+        params: any,
+        event: { stopPropagation: () => void },
+    ) => {
+        event.stopPropagation(); // Prevent deselection during editing
+    };
+
+    const addNewRow = () => {
+        let newRows = [...rows];
+        const newRow = {
+            id: self.crypto.randomUUID(),
+            warehouseId: warehouseId,
+            templateItems: [
+                {
+                    id: self.crypto.randomUUID(),
+                },
+            ],
+        };
+        newRows.push(newRow);
+        setRows(newRows);
+        setSelectedTemplate(newRow);
+    };
+
+    const Footer = (props: any) => {
+        return (
+            <Grid container>
+                <StyledTemplatesTableFooter>
+                    <Button
+                        title="Add row"
+                        onClick={() => {
+                            addNewRow();
+                        }}
+                    ></Button>
+                    <GridPagination {...props}/>
+                </StyledTemplatesTableFooter>
+            </Grid>
+        );
+    };
+
+    return (
+        <>
+            <StyledTemplateTable>
+                <StyledTemplateTableHeader>
+                    <h3>Templates</h3>
+                </StyledTemplateTableHeader>
+                <ThemeProvider theme={MuiTheme}>
+                    <StyledTemplatesTableContainer>
+                        <DataGrid
+                            rows={rows}
+                            columns={columns}
+                            autoHeight={true}
+                            checkboxSelection={true}
+                            disableMultipleRowSelection
+                            disableRowSelectionOnClick
+                            onCellEditStart={handleCellEditStart}
+                            processRowUpdate={handleProcessRowUpdate}
+                            onRowSelectionModelChange={handleRowSelectionModelChange}
+                            pagination
+                            pageSizeOptions={[5, 10, 25]}
+                            initialState={{
+                                pagination: {paginationModel: {pageSize: 5}},
+                            }}
+                            slots={{
+                                footer: Footer,
+                            }}
+                        />
+                        {/*<Footer/>*/}
+                    </StyledTemplatesTableContainer>
+                </ThemeProvider>
+            </StyledTemplateTable>
+        </>
+    );
 };
